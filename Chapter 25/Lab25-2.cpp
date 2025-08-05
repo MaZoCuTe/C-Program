2cc39a6403b1b63c823bfd2962cdb37ccffe0dc5
MaZoCuTe <153476005+MaZoCuTe@users.noreply.github.com>
2025-08-05 11:36:55 +0700

    Update Lab25-2.cpp

--- Chapter 25/Lab25-2.cpp
+++ Chapter 25/Lab25-2.cpp
@@ -1,1 +1,68 @@
-xxx
+#include <iostream>
+#include <cmath>
+
+class Base_class{
+	public:
+	int base, height;
+	int width, length;
+	double radius;	
+	
+	void findArea() {
+		std::cout << "FindARea()in Base class" << std::endl;
+	}
+	void perimeter() {
+		std::cout << "perimeter()in Base class" << std::endl;
+	}
+};
+class Triangle : public Base_class {
+	public:
+	Triangle(){
+		base = 10; length = 2;
+	}
+		void findArea(){
+			std::cout << "Area of triangle is " << (0.5 * base * height) << std::endl;
+		}
+		void perimeter() {
+			double c= std::sqrt((base*base) + (height*height));
+			std::cout << "Perimeter of triangle is " << (base + height + c) << std::endl;
+		}
+};
+class Rectangle : public Base_class {
+	public:
+	Rectangle(){
+		base = 5; length = 10;
+	}
+		void findArea(){
+			std::cout << "Area of rectangle is " << (width * length) << std::endl;
+		}
+		void perimeter(){
+			std::cout << "Perimeter of rectangle is " << (2*(width+length)) << std::endl;
+		}
+};
+class Circle : public Base_class {
+	public:
+	Circle(double rr){
+		radius = rr;
+	}
+	void findArea() {
+		std::cout << "Area of circle is " << (M_PI * radius * radius) << std::endl;
+	}
+	void perimeter() {
+		std::cout << "Perimeter of circle is " << (2.0 * M_PI * radius) << std::endl;
+	}
+};
+
+int main() {
+	Triangle tr;
+	
+	tr.findArea();
+	tr.perimeter();
+	
+	Rectangle rec;
+	rec.findArea();
+	rec.perimeter();
+	
+	Circle c(20);
+	c.findArea();
+	c.perimeter();
+}
